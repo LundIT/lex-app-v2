be1e2e10071c74aaaf54575a875004e8a26fc521
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-01-11 11:42:50 +0100

    Update name of the package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dpag",
+    name="dpag-admin",
     version="1.0.0",
     author="Melih Sünbül",
     author_email="m.sunbul@lund-it.com",
