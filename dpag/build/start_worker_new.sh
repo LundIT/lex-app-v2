a7f76b94360a524ea47be17ac0ac400caec7f5fa
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-02-21 17:11:32 +0100

    removed unnecessary directories

--- dpag/build/start_worker_new.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /app/DjangoProcessAdminGeneric/
-build/wait-for-it.sh -t 60 $DATABASE_DOMAIN:5432 -- celery -A DjangoProcessAdminGeneric  worker -l info --concurrency=4 --prefetch-multiplier=1
