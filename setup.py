5e5f53a538fe5927b36c189cd14c056b2f71f64b
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-03-25 13:21:46 +0100

    removed generic app and rest api reqs

--- setup.py
+++ setup.py
@@ -21,8 +21,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "generic_app @ git+https://github.com/LundIT/dpag-generic-app",
-        "ProcessAdminRestApi @ git+https://github.com/LundIT/dpag-rest-api",
         "wheel",
         "django~=4.1.10",
         "django-rest-framework",
