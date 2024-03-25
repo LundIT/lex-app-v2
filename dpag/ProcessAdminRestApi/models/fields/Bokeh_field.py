509f517e82d1bab553797d7a52aac9c5c39c5ac7
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-03-25 15:24:54 +0100

    Revert "added rest api and generic app inside dpag package"
    This reverts commit 7cbbb1cf580a9e704c5ca1c9ea209f319e2e6c38.

--- dpag/ProcessAdminRestApi/models/fields/Bokeh_field.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import TextField
-
-
-class BokehField(TextField):
-    max_length = 1000
-    pass
--- dpag/generic_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib.auth.admin import UserAdmin
