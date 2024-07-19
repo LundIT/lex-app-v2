4f2a1b91389921ad2bc052b008e017694568ec66
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-07-19 11:16:26 +0200

    disabled caching

--- lex/react/views.py
+++ lex/react/views.py
@@ -1,15 +1,11 @@
 import os
-
-from django.shortcuts import render
-
-# Create your views here.
 import posixpath
 from pathlib import Path
 
+from django.http import HttpResponse
 from django.utils._os import safe_join
 from django.views.static import serve as static_serve
 from lex.lex_app import settings
-from django.http import HttpResponse
 
 def serve_react(request, path, document_root=None):
     path = posixpath.normpath(path).lstrip("/")
@@ -25,21 +21,35 @@ def serve_react(request, path, document_root=None):
                 'REACT_APP_KEYCLOAK_REALM': os.getenv('KEYCLOAK_REALM'),
                 'REACT_APP_KEYCLOAK_URL': os.getenv('KEYCLOAK_URL'),
                 'REACT_APP_KEYCLOAK_CLIENT_ID': os.getenv('KEYCLOAK_CLIENT_ID'),
-                'REACT_APP_STORAGE_TYPE': os.getenv('STORAGE_TYPE', "LEGACY"),  # Defaults to "SHAREPOINT" if not set
+                'REACT_APP_STORAGE_TYPE': os.getenv('STORAGE_TYPE', "LEGACY"),
                 'REACT_APP_DOMAIN_BASE': os.getenv("REACT_APP_DOMAIN_BASE", "localhost"),
                 'REACT_APP_PROJECT_DISPLAY_NAME': os.getenv('PROJECT_DISPLAY_NAME', settings.repo_name),
                 'REACT_APP_GRAFANA_DASHBOARD_URL': os.getenv("REACT_APP_GRAFANA_DASHBOARD_URL", "localhost"),
             }
         }
 
-        # Dynamically replace 'undefined' with actual values in the script
         for key, value in replacements['undefined'].items():
             content = content.replace(f"window.{key} = undefined", f"window.{key} = \"{value}\"")
 
-        return HttpResponse(content, content_type='application/javascript')
+        response = HttpResponse(content, content_type='application/javascript')
+        # Set no-cache headers
+        response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
 
     fullpath = Path(safe_join(document_root, path))
     if fullpath.is_file():
-        return static_serve(request, path, document_root)
+        response = static_serve(request, path, document_root)
+        # Set no-cache headers for static files
+        response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
     else:
-        return static_serve(request, "index.html", document_root)
+        response = static_serve(request, "index.html", document_root)
+        # Set no-cache headers for index.html
+        response['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response['Pragma'] = 'no-cache'
+        response['Expires'] = '0'
+        return response
