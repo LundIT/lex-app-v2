4a46fa6ecdba120984030430e1e7da1f0506fccd
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-07-26 01:03:29 +0200

    streamlit tests

--- lex/streamlit_app.py
+++ lex/streamlit_app.py
@@ -1,32 +1,10 @@
 import os
-import sys
-
-from pathlib import Path
 import streamlit as st
 
-import django
 from streamlit_keycloak import login
 import traceback
 
-
-LEX_APP_PACKAGE_ROOT = Path(__file__).resolve().parent.as_posix()
-PROJECT_ROOT_DIR = Path(os.getcwd()).resolve()
-
-sys.path.append(LEX_APP_PACKAGE_ROOT)
-
-# The DJANGO_SETTINGS_MODULE has to be set to allow us to access django imports
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "lex_app.settings"
-)
-os.environ.setdefault(
-    "PROJECT_ROOT", PROJECT_ROOT_DIR.as_posix()
-)
-
-django.setup()
-
-
 if __name__ == '__main__':
-    import sys
     from lex_app.auth_helpers import resolve_user
     from lex_app.settings import repo_name
     try:
