0eaa5eaad53f7553586681c5e35678c29105ce3d
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-08-26 17:38:26 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,25 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     install_requires = f.read().splitlines()
 
+class CustomInstallCommand(install):
+    def run(self):
+        # First, run the standard installation
+        install.run(self)
+
+        # Now handle the custom installation of other_directory
+        self.move_other_directory()
+
+    def move_other_directory(self):
+        # Define the source and target paths
+        source = os.path.join(os.path.dirname(__file__), 'lex', 'generic_app')
+        target = os.path.join(os.path.dirname(self.install_lib), 'generic_app')
+
+        # Ensure the package_data entry points to the correct location
+        if os.path.exists(target):
+            shutil.rmtree(target)  # Remove the existing directory if it exists
+        shutil.move(source, target)
+        print(f'Moved other_directory to {target}')
+
 setup(
     name="lex-app",
     version="1.0.0",
@@ -12,6 +31,13 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/LundIT/lex-app",
     packages=find_packages(),
+    package_data={
+        '': ['generic_app/*'],  # Track the moved directory for cleanup
+    },
+    cmdclass={
+        'install': CustomInstallCommand,
+    },
+
     include_package_data=True,
     entry_points={
         'console_scripts': [
