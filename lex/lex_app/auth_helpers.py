197e210767f77bc433c35636a5f600356a2d154d
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-11-11 09:56:17 +0100

    new version with fixes and improvements

--- lex/lex_app/auth_helpers.py
+++ lex/lex_app/auth_helpers.py
@@ -1,16 +1,17 @@
+import os
+
 from django.contrib.auth.models import User, Group
 from sentry_sdk import set_user
-
-ADMIN = 'admin'
-STANDARD = 'standard'
-VIEW_ONLY = 'view-only'
-
+from generic_app.rest_api.views.lex_api.LexAPI import get_client_roles
 
 def resolve_user(request, id_token, rbac=True):
     # ask graph if logged in user is in a group /me/memberOf
     # want to see group 6d558e06-309d-4d6c-bb50-54f37a962e40
     # in http://graph.microsoft.com/v1.0/me/memberOf
     # in request._request.headers._store['authorization'] is auth header
+    if os.getenv("DEPLOYMENT_ENVIRONMENT"):
+        response = get_client_roles()
+        client_roles = response['roles']
     set_user({"name": id_token['name'], "email": id_token['email']})
     user, _ = User.objects.get_or_create(username=id_token['sub'])
     user.email = id_token['email']
@@ -21,21 +22,15 @@ def resolve_user(request, id_token, rbac=True):
         user.roles = user_roles
         user.save()
 
-        if all(item not in user_roles for item in [ADMIN, STANDARD, VIEW_ONLY]):
-            return None
+        if os.getenv("DEPLOYMENT_ENVIRONMENT"):
+            if all(item not in user_roles for item in client_roles):
+                return None
 
-        # Create or get existing groups
-        admin_group, created = Group.objects.get_or_create(name=ADMIN)
-        standard_group, created = Group.objects.get_or_create(name=STANDARD)
-        view_only_group, created = Group.objects.get_or_create(name=VIEW_ONLY)
+            for role in client_roles:
+                temp_group, created = Group.objects.get_or_create(name=role)
+                if role in user_roles and temp_group not in user.groups.all():
+                    user.groups.add(temp_group)
 
-        # Assign user to django groups according to KeyCloak data
-        if ADMIN in user_roles and admin_group not in user.groups.all():
-            user.groups.add(admin_group)
-        if STANDARD in user_roles and standard_group not in user.groups.all():
-            user.groups.add(standard_group)
-        if VIEW_ONLY in user_roles and view_only_group not in user.groups.all():
-            user.groups.add(view_only_group)
     user.save()
 
     return user
