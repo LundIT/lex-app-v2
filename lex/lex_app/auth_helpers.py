157835584ad1cba2dcc770b6dbe7bd13965aa251
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2025-05-12 12:04:00 +0200

    fixed import issue on auth_helpers.py

--- lex/lex_app/auth_helpers.py
+++ lex/lex_app/auth_helpers.py
@@ -3,67 +3,16 @@ import requests
 from keycloak.keycloak_openid import KeycloakOpenID
 from django.contrib.auth.models import User, Group
 from sentry_sdk import set_user
-from generic_app.rest_api.views.lex_api.LexAPI import get_client_roles
 
 ADMIN = 'admin'
 STANDARD = 'standard'
 VIEW_ONLY = 'view-only'
-
-def get_tokens_and_permissions(request):
-    access_token = request.headers["Authorization"].split("Bearer ")[-1]
-
-    keycloak_url = f"{os.getenv('KEYCLOAK_URL')}/realms/{os.getenv('KEYCLOAK_REALM')}/protocol/openid-connect/token"
-
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-
-    data = {
-        "client_id": os.getenv('KEYCLOAK_CLIENT_ID'),
-        "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
-        "subject_token": access_token,
-        "requested_token_type": "urn:ietf:params:oauth:token-type:access_token",
-        "audience": os.getenv('KEYCLOAK_CONFIDENTIAL_CLIENT_ID', 'LEX_LOCAL_ENV_CONFIDENTIAL')
-    }
-
-    response = requests.post(keycloak_url, headers=headers, data=data)
-    print(response.json())
-    confidential_access_token = response.json()["access_token"]
-
-    keycloak_openid = KeycloakOpenID(server_url=os.getenv('KEYCLOAK_URL') + "/",
-                                     realm_name=os.getenv('KEYCLOAK_REALM'),
-                                     client_id=os.getenv('KEYCLOAK_CONFIDENTIAL_CLIENT_ID', 'LEX_LOCAL_ENV_CONFIDENTIAL'),
-                                     client_secret_key=os.getenv('KEYCLOAK_CONFIDENTIAL_CLIENT_SECRET', '6jriqnnNsKPoJgzXOAU9TbwVwAkVlDJn'),
-                                     verify=True)
-    for item in keycloak_openid.uma_permissions(token=confidential_access_token):
-        print(item)
-    permissions = {item['rsid']: item.get('scopes', []) for item in
-                   keycloak_openid.uma_permissions(token=confidential_access_token)}
-    print(request.user.__dict__)
-    return {
-            "access_token": access_token,
-            "confidential_access_token": confidential_access_token,
-            "roles": request.user.roles,
-            "permissions": permissions,
-            "token": response.json()}
-
-def get_user_info(request):
-    return {"user":
-                {
-                    "name": request.user.name,
-                    "email": request.user.email
-                 },
-            "roles": request.user.roles,
-            "permissions": get_tokens_and_permissions(request)["permissions"]}
 
 def resolve_user(request, id_token, rbac=True):
     # ask graph if logged in user is in a group /me/memberOf
     # want to see group 6d558e06-309d-4d6c-bb50-54f37a962e40
     # in http://graph.microsoft.com/v1.0/me/memberOf
     # in request._request.headers._store['authorization'] is auth header
-    if os.getenv("DEPLOYMENT_ENVIRONMENT"):
-        response = get_client_roles()
-        client_roles = response['roles']
     set_user({"name": id_token['name'], "email": id_token['email']})
     user, _ = User.objects.get_or_create(username=id_token['sub'])
     user.email = id_token['email']
@@ -75,15 +24,21 @@ def resolve_user(request, id_token, rbac=True):
         user.roles = user_roles
         user.save()
 
-        if os.getenv("DEPLOYMENT_ENVIRONMENT"):
-            if all(item not in user_roles for item in client_roles):
-                return None
+        if all(item not in user_roles for item in [ADMIN, STANDARD, VIEW_ONLY]):
+            return None
 
-            for role in client_roles:
-                temp_group, created = Group.objects.get_or_create(name=role)
-                if role in user_roles and temp_group not in user.groups.all():
-                    user.groups.add(temp_group)
+        # Create or get existing groups
+        admin_group, created = Group.objects.get_or_create(name=ADMIN)
+        standard_group, created = Group.objects.get_or_create(name=STANDARD)
+        view_only_group, created = Group.objects.get_or_create(name=VIEW_ONLY)
 
+        # Assign user to django groups according to KeyCloak data
+        if ADMIN in user_roles and admin_group not in user.groups.all():
+            user.groups.add(admin_group)
+        if STANDARD in user_roles and standard_group not in user.groups.all():
+            user.groups.add(standard_group)
+        if VIEW_ONLY in user_roles and view_only_group not in user.groups.all():
+            user.groups.add(view_only_group)
     user.save()
 
     return user
