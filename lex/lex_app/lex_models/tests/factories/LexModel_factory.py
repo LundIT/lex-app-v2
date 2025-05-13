b1a3175c2457291d8922f7b0ac7c8b53e5727a1f
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-05-13 17:12:10 +0200

    Test editing

--- lex/lex_app/lex_models/tests/factories/LexModel_factory.py
+++ lex/lex_app/lex_models/tests/factories/LexModel_factory.py
@@ -1,3 +1,4 @@
+from django.db import models
 import factory
 
 from lex.lex_app.lex_models.LexModel import LexModel
@@ -7,6 +8,8 @@ class DummyLexModel(LexModel):
     class Meta:
         app_label = "lex_app"
         managed = True
+
+    dummy_field = models.CharField()
 
 
 class LexModelFactory(factory.django.DjangoModelFactory):
--- lex/lex_app/lex_models/tests/test_LexModel.py
+++ lex/lex_app/lex_models/tests/test_LexModel.py
@@ -11,7 +11,14 @@ from lex.lex_app.lex_models.tests.models.abstract_model_test_case import (
 class TestLexModel(AbstractModelTestCase):
     test_model = DummyLexModel
 
-    def test_default_fields(self):
+    def test_initial_instantiation(self):
         model = LexModelFactory.create()
         self.assertEqual(model.created_by, "Initial Data Upload")
         self.assertEqual(model.edited_by, None)
+
+    def test_edit_instance(self):
+        model = LexModelFactory.create()
+        model.dummy_field = "New Value"
+        model.save()
+        self.assertEqual(model.created_by, "Initial Data Upload")
+        self.assertEqual(model.edited_by, "Initial Data Upload")
